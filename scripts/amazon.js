f603658eb0b9d207f3230e7190fc8588bcfcaf93
isaacnicomedez <isaacnicomedez1@gmail.com>
2026-04-07 23:00:08 +0800

    Use id dataset to handle product quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach(product => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -60,13 +60,27 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-cart").forEach(btn => {
     btn.addEventListener('click', () => {
-        const productName = btn.dataset.productName;
-        cart.push(
-            {
-                productName: productName,
-                quantity: 1
+        const productId = btn.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach(item => {
+            if (productId === item.productId) {
+                matchingItem = item;
             }
-        );
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push(
+                {
+                    productId: productId,
+                    quantity: 1
+                }
+            );
+        }
+
         console.log(cart);
     })
 });
