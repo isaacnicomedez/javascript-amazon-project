24e926ee9d677350ed6bb1d8df924a1533eaab99
isaacnicomedez <isaacnicomedez1@gmail.com>
2026-04-08 12:29:49 +0800

    Refactor to synchronize manageable executions

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,93 +1,113 @@
+function main() {
+  calculateCartQuantity();
+  generateHTML();
+  pushOrder();
+}
+
+
 // Generate HTML
-let productsHTML = '';
+function generateHTML() {
+  let productsHTML = '';
 
-products.forEach(product => {
-    productsHTML += `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  products.forEach(product => {
+      productsHTML += `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              $${(product.priceCents / 100).toFixed(2)}
+            </div>
+
+            <div class="product-quantity-container">
+              <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+              Add to Cart
+            </button>
           </div>
+      `;
+  });
 
-          <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
-          </div>
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+}
 
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>
-    `;
-});
-
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 // Make it interactive
+function pushOrder() {
+  document.querySelectorAll(".js-add-to-cart")
+    .forEach(btn => {
+      btn.addEventListener('click', () => {
+          const productId = btn.dataset.productId;
 
-document.querySelectorAll(".js-add-to-cart").forEach(btn => {
-    btn.addEventListener('click', () => {
-        const productId = btn.dataset.productId;
+          // Push product or increment quantity
+          let matchingItem;
 
-        // Add item or increment quantity
-        let matchingItem;
+          cart.forEach(item => {
+              if (productId === item.productId) {
+                  matchingItem = item;
+              }
+          });
 
-        cart.forEach(item => {
-            if (productId === item.productId) {
-                matchingItem = item;
-            }
-        });
+          if (matchingItem) {
+              matchingItem.quantity += 1;
+          } else {
+              cart.push(
+                  {
+                      productId: productId,
+                      quantity: 1
+                  }
+              );
+          }
 
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            cart.push(
-                {
-                    productId: productId,
-                    quantity: 1
-                }
-            );
-        }
+          calculateCartQuantity();
+      })
+    });
+}
 
-        let cartQuantity = 0;
 
-        cart.forEach(item => {
-            cartQuantity += item.quantity; 
-        });
+function calculateCartQuantity() {
+  let cartQuantity = 0;
 
-        document.querySelector(".js-cart-quantity").textContent = cartQuantity;
-    })
-});
+  cart.forEach(item => {
+      cartQuantity += item.quantity; 
+  });
+
+  document.querySelector(".js-cart-quantity").textContent = cartQuantity;
+}
+
+
+main();
