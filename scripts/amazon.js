8b1ce8a0a09d99d5a2af0e784620de5980f63701
isaacnicomedez <isaacnicomedez1@gmail.com>
2026-04-09 14:00:25 +0800

    Modularized currency formatter

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let showMessage = {};
 
@@ -34,7 +35,7 @@ function generateHTML() {
             </div>
 
             <div class="product-price">
-              $${(product.priceCents / 100).toFixed(2)}
+              $${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
